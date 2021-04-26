1074c60e70a6a56eab016adfad7eb9eaff7fab15
Marcin Górski <m.gorski@marketlab.pl>
2021-04-26 16:34:19 +0200

    WIP: reset starting point for revision.
    GitLab CI clones only with depth 50, so for testing reasons
    Change starting point to a recent one.

--- scripts/getver.sh
+++ scripts/getver.sh
@@ -12,7 +12,7 @@ try_version() {
 }
 
 try_git() {
-	REBOOT=ee53a240ac902dc83209008a2671e7fdcf55957a
+	REBOOT=f336af178ecd99f73121ae19eaa46aa2f5e9bcf7
 	git rev-parse --git-dir >/dev/null 2>&1 || return 1
 
 	[ -n "$GET_REV" ] || GET_REV="HEAD"
